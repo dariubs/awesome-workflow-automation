5585343d7a12c1372f80e44899a822173c7fe9fc
Dariush Abbasi <dariushem@yahoo.com>
2014-10-18 15:20:13 +0330

    some style

--- js/rtl.js
+++ js/rtl.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
-    $('textarea').autosize();
-   
-   	$('body').append('<div id="content"></div>');
 
-	
-    $("#box").on('keydown', function() {
-    	var content = $('#box').val();
-    	$('#content').html(marked(content));
+
+(function(){
+
+	$(document).ready(function(){
+	    $('textarea').autosize();
+	   	$('body').append('<div id="content"></div>');
+	   	var content = $('#box').val();
+	    $('#content').html(marked(content));
+	    
+
+	    $("#box").on('keydown', function() {
+	    	var content = $('#box').val();
+	    	$('#content').html(marked(content));
+		});
 	});
 
-
-	
-	
-
-});
-
+}())
