594583c314005ab767a8e1d4f86fa8725bf44b8c
Dariush Abbasi <dariushem@yahoo.com>
2014-10-22 17:18:50 +0330

    add gAnalytics and colored code

--- js/rtl.js
+++ js/rtl.js
@@ -5,7 +5,7 @@
 	var initUrl = 'http://dariubs.github.io/rtlmd/initcontent.md';
 
 	$(document).ready(function(){
-		
+
 	   	$.get(initUrl)
 			.done(function( data ) {
 				$('textarea').val(data);
@@ -25,4 +25,13 @@
 		}, 2000);
 	});
 
-}())
+}());
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-55991804-1', 'auto');
+ga('send', 'pageview');
